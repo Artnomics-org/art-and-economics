02a4e1fa3369f6eaffcb61a20117e1015fe88831
Garfield Lee <openpgp@550.moe>
2021-06-24 22:30:10 +0800

    feat: better pool page

--- src/views/Pool/Pool.tsx
+++ src/views/Pool/Pool.tsx
@@ -1,32 +1,94 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Page from '../../components/Page'
-import { Button, ButtonWrapper, Content, Info, Link, PoolWrapper, Title } from './components/styleds'
+import {
+  Button,
+  ButtonWrapper,
+  Content,
+  Info,
+  InternalLink,
+  Link,
+  PoolWrapper,
+  PositionCardWrapper,
+  Title,
+  VoteCard,
+} from './components/styleds'
 import { IconArrow } from '../Swap/components/styleds'
 import IconLongArrow from '../../assets/img/icon-long-arrow.svg'
+import { useActiveWeb3React } from '../../hooks/wallet'
+import { toV2LiquidityToken, usePairs, useTrackedTokenPairs } from '../../hooks/liquidity'
+import { useTokenBalancesWithLoadingIndicator } from '../../hooks/token'
+import { Pair } from '@haneko/uniswap-sdk'
+import FullPositionCard from '../../components/PositionCard'
 
 const Pool: React.FC = () => {
+  const { account } = useActiveWeb3React()
+  // fetch the user's balances of all tracked V2 LP tokens
+  const trackedTokenPairs = useTrackedTokenPairs()
+  const tokenPairsWithLiquidityTokens = useMemo(
+    () => trackedTokenPairs.map(tokens => ({ liquidityToken: toV2LiquidityToken(tokens), tokens })),
+    [trackedTokenPairs]
+  )
+  const liquidityTokens = useMemo(() => tokenPairsWithLiquidityTokens.map(tpwlt => tpwlt.liquidityToken), [
+    tokenPairsWithLiquidityTokens
+  ])
+  const [v2PairsBalances, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(
+    account,
+    liquidityTokens
+  )
+  // fetch the reserves for all V2 pools in which the user has a balance
+  const liquidityTokensWithBalances = useMemo(
+    () =>
+      tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
+        v2PairsBalances[liquidityToken.address]?.greaterThan('0')
+      ),
+    [tokenPairsWithLiquidityTokens, v2PairsBalances]
+  )
+  const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens))
+  const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair))
+
+  const isLoading = account && (fetchingV2PairBalances || v2Pairs?.length < liquidityTokensWithBalances.length || v2Pairs?.some(V2Pair => !V2Pair))
+  const isNoLiquidity = account && !isLoading && !allV2PairsWithLiquidity.length
+
+  console.log(allV2PairsWithLiquidity)
+
   return (
     <Page>
       <PoolWrapper>
         <IconArrow src={IconLongArrow} alt="icon arrow" />
-        <Title>Liquidity provider rewards</Title>
-        <Content>
-          Liquidity providers earn a 0.3% fee on all trades proportional to
-          <br />
-          their share of the pool. Fees are added to the pool, accrue in real
-          <br />
-          time and can be claimed by withdrawing your liquidity.
-          <br />
-        </Content>
-        <Link href="#" style={{ width: '100%', marginBottom: '40px' }}>
-          Read more about providing liquidity
-        </Link>
+        <VoteCard>
+          <Title>Liquidity provider rewards</Title>
+          <Content>
+            Liquidity providers earn a 0.3% fee on all trades proportional to
+            <br />
+            their share of the pool. Fees are added to the pool, accrue in real
+            <br />
+            time and can be claimed by withdrawing your liquidity.
+            <br />
+          </Content>
+          <Link
+            target="_blank"
+            href="https://uniswap.org/docs/v2/core-concepts/pools/"
+            rel="noopener noreferrer"
+            style={{ width: '100%' }}
+          >
+            Read more about providing liquidity
+          </Link>
+        </VoteCard>
         <ButtonWrapper>
           <Button style={{ marginRight: '32px' }}>Create a pair</Button>
           <Button>Add liquidity</Button>
         </ButtonWrapper>
-        <Info style={{ marginBottom: '48px' }}>No liquidity found.</Info>
-        <Content>Don&#39;t see a pool you joined? <Link>Import it.</Link></Content>
+        <PositionCardWrapper>
+          {!account && <Info>Connect to a wallet to view your liquidity.</Info>}
+          {isLoading && <Info>Loading...</Info>}
+          {isNoLiquidity && <Info>No liquidity found.</Info>}
+          {allV2PairsWithLiquidity.map(v2Pair => (
+            <FullPositionCard key={v2Pair.liquidityToken.address} pair={v2Pair} />
+          ))}
+        </PositionCardWrapper>
+        <Content>
+          Don&#39;t see a pool you joined? <InternalLink to='/find'>Import it.</InternalLink>
+        </Content>
       </PoolWrapper>
     </Page>
   )
--- src/views/Pool/components/styleds.tsx
+++ src/views/Pool/components/styleds.tsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components/macro'
+import { Link as RouterLink } from 'react-router-dom'
 
 export const PoolWrapper = styled.div`
   flex: 1;
   max-width: ${props => props.theme.siteWidth}px;
   padding-top: 94px;
   padding-bottom: 82px;
+`
+
+export const VoteCard = styled.div`
+  width: 100%;
+  margin-bottom: 40px;
 `
 
 export const Title = styled.h1`
@@ -70,4 +76,23 @@ export const Info = styled.p`
   text-align: center;
   text-transform: uppercase;
   color: ${(props) => props.theme.color.grey[600]};
+`
+
+export const InternalLink = styled(RouterLink)`
+  display: inline-block;
+  font-family: 'Helvetica Neue LT W05_53 Ext';
+  font-size: 16px;
+  line-height: 1.5;
+  letter-spacing: 0.6px;
+  text-align: center;
+  text-transform: uppercase;
+  color: ${(props) => props.theme.color.grey[600]};
+  text-decoration: underline;
+`
+
+export const PositionCardWrapper = styled.div`
+  max-width: 640px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 48px;
 `
