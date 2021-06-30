7945f63d272f17453f757d98c151c8d6f31608db
Garfield Lee <openpgp@550.moe>
2021-06-30 19:39:09 +0800

    chore: remove unused files

--- src/hooks/useNFTs.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { Context as NFTsContext } from '../contexts/NFTs'
-
-const useNFTs = () => {
-    const { nfts } = useContext(NFTsContext)
-    return [nfts]
-}
-
-export default useNFTs
--- src/utils/refReward.ts
+++
@@ -1,8 +0,0 @@
-import { Web3Provider } from '@ethersproject/providers'
-import ReferralRewardABI from '../constants/abi/ReferralReward.json'
-import { getContractWithAbi } from './ethers'
-
-export const getContract = (provider: Web3Provider, address: string) => {
-  const contract = getContractWithAbi(address, ReferralRewardABI, provider)
-  return contract
-}
