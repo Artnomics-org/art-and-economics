661054d372e6e3d3f512562b182460a0d0c02dfd
Garfield Lee <openpgp@550.moe>
2021-06-30 17:34:10 +0800

    feat: add utils

--- src/utils/lists.ts
+++ src/utils/lists.ts
@@ -74,3 +74,14 @@ export function isTokenOnList(defaultTokens: TokenAddressMap, currency?: Currenc
   return Boolean(currency instanceof Token && defaultTokens[currency.chainId]?.[currency.address])
 }
 
+// chunks array into chunks of maximum size
+// evenly distributes items among the chunks
+export function chunkArray<T>(items: T[], maxChunkSize: number): T[][] {
+  if (maxChunkSize < 1) throw new Error('maxChunkSize must be gte 1')
+  if (items.length <= maxChunkSize) return [items]
+
+  const numChunks: number = Math.ceil(items.length / maxChunkSize)
+  const chunkSize = Math.ceil(items.length / numChunks)
+
+  return [...Array(numChunks).keys()].map(ix => items.slice(ix * chunkSize, ix * chunkSize + chunkSize))
+}
