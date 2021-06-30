17ff1ecafba1326baf6d64ee5ea666af86189551
Garfield Lee <openpgp@550.moe>
2021-06-30 17:34:58 +0800

    feat: add updaters

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,35 @@ import { getLibrary } from './utils'
 import { NetworkContextName } from './constants'
 import store from './state'
 import theme from './theme'
+import Web3ReactManager from './components/Web3ReactManager'
+import { useAppInactiveListener } from './hooks/connector'
+
+import { ApplicationUpdater } from './updaters/application'
+import { ListsUpdater } from './updaters/lists'
+import { MulticallUpdater } from './updaters/multicall'
+import { UserUpdater } from './updaters/user'
 
 import Farms from './views/Farms'
 import Home from './views/Home'
 import NFTs from './views/NFTs'
 import Swap from './views/Swap'
 import Pool from './views/Pool'
-import Web3ReactManager from './components/Web3ReactManager'
 import AddLiquidity from './views/AddLiquidity'
-import { useInactiveListener } from './hooks/connector'
+
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
 const App: React.FC = () => {
   return (
     <Providers>
+      <Updaters />
       <Routers />
     </Providers>
   )
 }
 
 const Routers: React.FC = () => {
-  useInactiveListener()
+  useAppInactiveListener()
 
   return (
     <Web3ReactManager>
@@ -51,6 +58,17 @@ const Routers: React.FC = () => {
         </Switch>
       </Router>
     </Web3ReactManager>
+  )
+}
+
+const Updaters: React.FC = () => {
+  return (
+    <>
+      <ListsUpdater />
+      <UserUpdater />
+      <ApplicationUpdater />
+      <MulticallUpdater />
+    </>
   )
 }
 
