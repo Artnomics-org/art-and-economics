441ba626bd1bd4b298eef1500e5eb3036ac1f0b3
Garfield Lee <openpgp@550.moe>
2021-07-07 18:10:55 +0800

    feat: add constants

--- src/constants/ethers.ts
+++ src/constants/ethers.ts
@@ -4,6 +4,7 @@ import ENS_ABI from './abi/ens-registrar.json'
 import ENS_PUBLIC_RESOLVER_ABI from './abi/ens-public-resolver.json'
 import ERC20_BYTES32_ABI from './abi/erc20_bytes32.json'
 import WETH_ABI from './abi/weth.json'
+import { abi as IUniswapV2Router02ABI } from '@uniswap/v2-periphery/build/IUniswapV2Router02.json'
 
 const ERC20_INTERFACE = new Interface(ERC20_ABI.abi)
 const ERC20_BYTES32_INTERFACE = new Interface(ERC20_BYTES32_ABI)
@@ -64,4 +65,5 @@ export {
   ERC20_INTERFACE,
   ERC20_BYTES32_INTERFACE,
   WETH_ABI,
+  IUniswapV2Router02ABI,
 }
