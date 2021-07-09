c7a95dd0fe0cd6320d0e9c96286337f07011c3f4
Garfield Lee <openpgp@550.moe>
2021-07-09 16:34:20 +0800

    feat: complete MiniPositionCard component

--- src/components/PositionCard/MinimalPositionCard.tsx
+++ src/components/PositionCard/MinimalPositionCard.tsx
@@ -5,6 +5,11 @@ import { useActiveWeb3React } from '../../hooks/wallet'
 import { useTokenBalance, useTotalSupply } from '../../hooks/token'
 import { unwrappedToken } from '../../utils/currency'
 import { PositionCardProps } from './types'
+import { AutoColumn } from '../Column'
+import { FixedHeightRow } from './styleds'
+import { RowFixed } from '../Row'
+import { useCallback } from 'react'
+import { DoubleCurrencyLogo } from '../CurrencyLogo'
 
 const MinimalPositionCard: React.FC<PositionCardProps> = ({ pair, border, showUnwrapped = false }) => {
   const { account } = useActiveWeb3React()
@@ -29,7 +34,96 @@ const MinimalPositionCard: React.FC<PositionCardProps> = ({ pair, border, showUn
         ]
       : [undefined, undefined]
 
-  return <></>
+  const userHasBalance = userPoolBalance && JSBI.greaterThan(userPoolBalance.raw, JSBI.BigInt(0))
+  const posiText1 =
+    "By adding liquidity you'll earn 0.3% of all trades on this pair proportional to your share of the pool."
+  const posiText2 = 'Fees are added to the pool, accrue in real time and can be claimed by withdrawing your liquidity.'
+
+  const handleShowMore = useCallback(() => {
+    setShowMore(!showMore)
+  }, [showMore])
+
+  return (
+    <CardWrapper>
+      {userHasBalance ? (
+        <AutoColumn gap="12px">
+          <FixedHeightRow>
+            <NormalText>Your Position</NormalText>
+          </FixedHeightRow>
+          <FixedHeightRow onClick={handleShowMore}>
+            <RowFixed>
+              <DoubleCurrencyLogo currency0={currency0} currency1={currency1} margin={true} size={20} />
+              <BigText>
+                {currency0.symbol}/{currency1.symbol}
+              </BigText>
+            </RowFixed>
+            <RowFixed>
+              <BigText>{userPoolBalance ? userPoolBalance.toSignificant(4) : '-'}</BigText>
+            </RowFixed>
+          </FixedHeightRow>
+          <AutoColumn gap="4px">
+            <FixedHeightRow>
+              <NormalText>Your Pool Share:</NormalText>
+              <NormalText>{poolTokenPercentage ? poolTokenPercentage.toFixed(6) + '%' : '-'}</NormalText>
+            </FixedHeightRow>
+            <FixedHeightRow>
+              <NormalText>{currency0.symbol}:</NormalText>
+              {token0Deposited ? (
+                <RowFixed style={{ marginLeft: '6px' }}>
+                  <NormalText>{token0Deposited?.toSignificant(6)}</NormalText>
+                </RowFixed>
+              ) : (
+                '-'
+              )}
+            </FixedHeightRow>
+            <FixedHeightRow>
+              <NormalText>{currency1.symbol}:</NormalText>
+              {token1Deposited ? (
+                <RowFixed style={{ marginLeft: '6px' }}>
+                  <NormalText>{token1Deposited?.toSignificant(6)}</NormalText>
+                </RowFixed>
+              ) : (
+                '-'
+              )}
+            </FixedHeightRow>
+          </AutoColumn>
+        </AutoColumn>
+      ) : (
+        <SmallText>
+          {posiText1}
+          <br />
+          {posiText2}
+        </SmallText>
+      )}
+    </CardWrapper>
+  )
 }
+
+const CardWrapper = styled.div`
+  width: 100%;
+  border-radius: 13.5px;
+  border: solid 2px ${(props) => props.theme.color.grey[600]};
+  padding: 20px;
+  background-color: ${(props) => props.theme.color.bg};
+  position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+`
+
+const SmallText = styled.p`
+  margin: 0;
+  font-size: 14px;
+  font-weight: 400;
+`
+
+const NormalText = styled.p`
+  margin: 0;
+  font-size: 16px;
+  font-weight: 500;
+`
+
+const BigText = styled(NormalText)`
+  font-size: 20px;
+`
 
 export default MinimalPositionCard
