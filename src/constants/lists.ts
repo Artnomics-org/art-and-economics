2a31d2786acfa925b1a3eda445a37c594e48fc91
Garfield Lee <openpgp@550.moe>
2021-09-23 10:18:19 +0800

    feat: use @art-economics/default-token-list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,8 @@
 import { ChainId, Token, WETH } from '@art-economics/swap-sdk'
 
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://unpkg.com/best-default-token-list@1.9.23/build/tokens.json'
+export const DEFAULT_TOKEN_LIST_URL =
+  'https://unpkg.com/@art-economics/default-token-list@latest/build/ane-default.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
 
