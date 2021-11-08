c9e126bec9bf2cb57320a02761f8119a090973b4
Garfield Lee <openpgp@550.moe>
2021-11-09 03:56:10 +0800

    fix: redirect

--- src/views/Home/redirects.tsx
+++ src/views/Home/redirects.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { Redirect, RouteComponentProps } from 'react-router-dom'
 
 export const RedirectPathToHome: React.FC<RouteComponentProps> = ({ location }) => {
-  return <Redirect to={{ ...location, pathname: '/home' }} />
+  return <Redirect to={{ ...location, pathname: '/' }} />
 }
